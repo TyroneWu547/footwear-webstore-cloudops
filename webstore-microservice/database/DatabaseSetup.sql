cba61008e54d9997ec6e82586158c40d833dff93
tkwu <tkwu@ncsu.edu>
2022-11-13 22:35:30 -0500

    added privilege flush for grant error

--- webstore-microservice/database/DatabaseSetup.sql
+++ webstore-microservice/database/DatabaseSetup.sql
@@ -1,5 +1,5 @@
+FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'csc547'@'%';
-CREATE DATABASE IF NOT EXISTS Products;
 USE Products;
 CREATE OR REPLACE TABLE Orders (ItemID bigint(20) unsigned, CustomerEmail varchar(1024), Quantity int(10) unsigned);
 CREATE OR REPLACE TABLE Footwear (ItemID bigint(20) unsigned, Name varchar(1024), Description text, Cost decimal);
