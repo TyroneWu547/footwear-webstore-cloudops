6df6472b4a29bfacc31173bd39257cb37f69c1b0
tkwu <tkwu@ncsu.edu>
2022-11-16 17:41:35 -0500

    added inbound rule to access nodeport service

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,9 @@ resource "aws_key_pair" "ssh_key_pair" {
     public_key = tls_private_key.ssh_key_gen.public_key_openssh
 }
 
-# Inbound rule to allow SSH access to EC2 instances
-resource "aws_security_group" "ssh_http_https_access" {
-    name = "ssh-http-https-access"
+# Security rules to allow SSH access to and internet traffic from
+resource "aws_security_group" "ssh_internet_access" {
+    name = "ssh-internet-access"
     description = "Basic in/out rules for EC2 instances"
 
     ingress {
@@ -54,18 +54,49 @@ resource "aws_security_group" "ssh_http_https_access" {
     # }
 }
 
-# # EC2 Instance: database server
-# resource "aws_instance" "database_server" {
-#     ami = var.db_ec2.ami_type
-#     instance_type = var.db_ec2.instance_type
-#     key_name = aws_key_pair.ssh_key_pair.key_name
-#     vpc_security_group_ids = [ aws_security_group.ssh_http_https_access.id ]
+# Allow access to MySQL database
+resource "aws_security_group" "mysql_access" {
+    name = "mysql-access"
+    description = "Open access to mysql database"
 
-#     tags = {
-#         Name = "database_server"
-#         Description = "Database server"
-#     }
-# }
+    ingress {
+        description = "Inbound rule for mysql"
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Allow access to 'route' NodePort service
+resource "aws_security_group" "nodeport_access" {
+    name = "nodeport-access"
+    description = "Open access to NodePort service"
+
+    ingress {
+        description = "Inbound rule for NodePort service"
+        from_port = 30000
+        to_port = 30000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# EC2 Instance: database server
+resource "aws_instance" "database_server" {
+    ami = var.db_ec2.ami_type
+    instance_type = var.db_ec2.instance_type
+    key_name = aws_key_pair.ssh_key_pair.key_name
+    vpc_security_group_ids = [ 
+        aws_security_group.ssh_internet_access.id,
+        aws_security_group.mysql_access.id
+    ]
+
+    tags = {
+        Name = "database_server"
+        Description = "Database server"
+    }
+}
 
 # EC2 Instance: control node
 resource "aws_instance" "control_node" {
@@ -73,7 +104,8 @@ resource "aws_instance" "control_node" {
     instance_type = var.control_node_ec2.instance_type
     key_name = aws_key_pair.ssh_key_pair.key_name
     vpc_security_group_ids = [ 
-        aws_security_group.ssh_http_https_access.id 
+        aws_security_group.ssh_internet_access.id,
+        aws_security_group.nodeport_access.id
     ]
 
     tags = {
@@ -87,7 +119,7 @@ resource "aws_instance" "control_node" {
 #     ami = var.worker_nodes_ec2.ami_type
 #     instance_type = var.worker_nodes_ec2.instance_type
 #     key_name = aws_key_pair.ssh_key_pair.key_name
-#     vpc_security_group_ids = [ aws_security_group.ssh_http_https_access.id ]
+#     vpc_security_group_ids = [ aws_security_group.ssh_internet_access.id ]
 
 #     count = var.worker_nodes_ec2.instance_count
 
@@ -101,11 +133,12 @@ resource "aws_instance" "control_node" {
 resource "local_file" "kube_cluster_hosts" {
     filename = "../ansible/inventory/inventory"
     content = templatefile(
-        "../ansible/inventory/inventory.tftpl", 
+        "../ansible/inventory/inventory-template.tftpl", 
         {
-            # database_server_ip = aws_instance.database_server.public_ip,
+            database_server_ip = aws_instance.database_server.public_ip,
             control_node_ip = aws_instance.control_node.public_ip,
             # worker_nodes_ip = aws_instance.control_node.*.public_ip
+            worker_nodes_ip = []
         }
     )
 }
