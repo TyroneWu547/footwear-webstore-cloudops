3bacf850c14caba50658e65de413788856dbf4cd
tkwu <tkwu@ncsu.edu>
2022-11-22 04:27:35 -0500

    added reqs for route/display & buy with rand data

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,10 +1,25 @@
 import time
+import random
 from locust import HttpUser, task, between
+
+# Valid Quantities in Route page
+quantities = [ 1, 2 ]
+# Valid item IDs in the inventory
+itemIDs = [ 1, 2, 3 ]
+# Sample emails
+customerEmails = [ "tkwu@ncsu.edu", "ajbulthu@ncsu.edu", "biudechu@ncsu.edu", "candice@ncsu.edu" ]
 
 class FootwearWebstoreUser(HttpUser):
     wait_time = between(0, 5)
 
     @task
-    def route_page(self):
-        # "/products.php" implicitly calls "/displayProducts.php"
+    def route_display_page(self):
+        # implicitly calls "/displayProducts.php"
         self.client.get(url="/products.php")
+    
+    @task
+    def buy_item(self):
+        quantity = random.choice(quantities)
+        itemID = random.choice(itemIDs)
+        customerEmail = random.choice(customerEmails)
+        self.client.get(url="/products.php?action=buy&quantity={}&itemID={}&customerEmail={}".format(quantity, itemID, customerEmail))
