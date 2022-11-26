505e67c742d858113668500f3f6b2bcff6ce6fdc
tkwu <tkwu@ncsu.edu>
2022-11-26 01:19:46 -0500

    added back nodeport in buy and display for locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,25 +1,30 @@
 import time
 import random
-from locust import HttpUser, task, between
+from locust import FastHttpUser, task, between
 
 # Valid Quantities in Route page
 quantities = [ 1, 2 ]
 # Valid item IDs in the inventory
 itemIDs = [ 1, 2, 3 ]
 # Sample emails
-customerEmails = [ "tkwu@ncsu.edu", "ajbulthu@ncsu.edu", "biudechu@ncsu.edu", "candice@ncsu.edu" ]
+customerEmails = [ "tkwu@ncsu.edu", "ajbulthu@ncsu.edu", 
+                    "biudechu@ncsu.edu", "candice@ncsu.edu" ]
 
-class FootwearWebstoreUser(HttpUser):
-    wait_time = between(0, 5)
+class FootwearWebstoreUser(FastHttpUser):
+    wait_time = between(1, 5)
 
     @task
-    def route_display_page(self):
-        # implicitly calls "/displayProducts.php"
-        self.client.get(url="/products.php")
+    def route_page(self):
+        self.client.get(url=":30000/products.php")
     
     @task
     def buy_item(self):
         quantity = random.choice(quantities)
         itemID = random.choice(itemIDs)
         customerEmail = random.choice(customerEmails)
-        self.client.get(url="/products.php?action=buy&quantity={}&itemID={}&customerEmail={}".format(quantity, itemID, customerEmail))
+        self.client.get(url=":30001/buyProducts.php?quantity={}&itemID={}&customerEmail={}"
+                            .format(quantity, itemID, customerEmail))
+    
+    @task
+    def display_inventory(self):
+        self.client.get(url=":30002/displayProducts.php")
